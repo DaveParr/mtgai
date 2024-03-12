bc26db89ab755eab42aa3bc5c79d4e131767e820
Dave Parr <8363743+DaveParr@users.noreply.github.com>
2024-03-12 16:41:44 +0000

    Setup project with cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experiments with MTG and AI',
+    author='"Dave Parr <8363743+DaveParr@users.noreply.github.com>"',
+    license='MIT',
+)
