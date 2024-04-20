e888b5c44d1c13f2c0468246317982fb3562de92
Dave Parr <8363743+DaveParr@users.noreply.github.com>
2024-04-20 16:10:22 +0100

    Redesign to vertical layout with reactivity steps based on workflow

--- src/application/app.py
+++ src/application/app.py
@@ -31,13 +31,38 @@ commander_names = commanders["name"].to_list()
 
 log = structlog.get_logger()
 
+llm = OpenAI()
 
-def card_suggestions(commander) -> tuple[pl.DataFrame, pl.DataFrame]:
+
+def get_commander_data(commander) -> pl.DataFrame:
+    return commanders.filter(pl.col("name") == commander).select(
+        ["name", "manaCost", "supertypes", "subtypes", "text", "power", "toughness"]
+    )
+
+
+def card_suggestions(commander) -> pl.DataFrame:
     # return the commander tha matches the name
     commander_data = commanders.filter(pl.col("name") == commander)
 
     mtg_card_prompt = PromptTemplate.from_template(
-        "Suggest a Magic the Gathering card that would be valuable to include in a commander deck with {commander} as the commander. Do not suggest the commander card itself."
+        """"
+        Design a Magic the Gathering card that would be valuable to include in a commander deck with {commander} as the commander. 
+        Do not suggest the commander card itself.
+        Your suggestions should be based on the commander's abilities and the general strategy of the deck.
+
+        Your response should be a card formated in json, each with the following information:
+        - Name
+        - Mana cost
+        - Supertypes
+        - Subtypes
+        - Text
+        - Power
+        - Toughness
+
+        Do not include the commander card in the suggestions.
+
+        Do not include any extra information in the response.
+        """
     )
 
     hyde_embeddings = HypotheticalDocumentEmbedder.from_llm(
@@ -47,40 +72,43 @@ def card_suggestions(commander) -> tuple[pl.DataFrame, pl.DataFrame]:
     hyde_result = hyde_embeddings.embed_query(commander_data.to_dict(as_series=False))
 
     hyde_search = vectorstore.similarity_search_by_vector_with_relevance_scores(
-        hyde_result, k=10
+        hyde_result, k=20
     )
 
-    metadata_list = [document[0].metadata for document in hyde_search]
+    suggestions = [document[0].metadata for document in hyde_search]
 
     # log if commander card name in metadata
-    for item in metadata_list:
+    for item in suggestions:
         if commander in item["name"]:
             log.info("Commander card in metadata", metadata=item)
 
-    display_commander = commander_data.select(["name", "manaCost", "type", "text"])
-
-    display_metadata = (
-        pl.DataFrame(metadata_list)
-        .select(["name", "manaCost", "type", "text"])
+    display_suggestions = (
+        pl.DataFrame(suggestions)
+        .select(
+            ["name", "manaCost", "supertypes", "subtypes", "text", "power", "toughness"]
+        )
         .filter(~pl.col("name").str.contains(commander))
     )
 
-    return display_commander, display_metadata
+    return display_suggestions
 
 
-demo = gr.Interface(
-    fn=card_suggestions,
-    inputs=gr.Dropdown(
-        commander_names, label="Comander", info="Select a commander for the deck"
-    ),
-    outputs=[  # commander data
-        gr.DataFrame(),
-        # metadata
-        gr.DataFrame(),
-    ],
-)
+with gr.Blocks() as demo:
+    with gr.Column():
+        commander = gr.Dropdown(
+            commander_names, label="Comander", info="Select a commander for the deck"
+        )
+        commander_display = gr.DataFrame()
+        generate_button = gr.Button("Generate")
+        suggestions_display = gr.DataFrame()
 
-llm = OpenAI()
+    commander.change(get_commander_data, inputs=commander, outputs=commander_display)
+
+    generate_button.click(
+        card_suggestions,
+        inputs=commander,
+        outputs=suggestions_display,
+    )
 
 
 if __name__ == "__main__":
