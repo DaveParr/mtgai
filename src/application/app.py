1115e1870cbeed27c01e26b296e780e2736f5092
Dave Parr <8363743+DaveParr@users.noreply.github.com>
2024-04-20 15:33:28 +0100

    Refactor card_suggestions function to return commander card data, but filter from search results if present

--- src/application/app.py
+++ src/application/app.py
@@ -32,11 +32,9 @@ commander_names = commanders["name"].to_list()
 log = structlog.get_logger()
 
 
-def card_suggestions(commander):
+def card_suggestions(commander) -> tuple[pl.DataFrame, pl.DataFrame]:
     # return the commander tha matches the name
-    commander_data = commanders.filter(pl.col("name") == commander).to_dict(
-        as_series=False
-    )
+    commander_data = commanders.filter(pl.col("name") == commander)
 
     mtg_card_prompt = PromptTemplate.from_template(
         "Suggest a Magic the Gathering card that would be valuable to include in a commander deck with {commander} as the commander. Do not suggest the commander card itself."
@@ -46,7 +44,7 @@ def card_suggestions(commander):
         llm=llm, base_embeddings=embeddings, custom_prompt=mtg_card_prompt
     )
 
-    hyde_result = hyde_embeddings.embed_query(commander_data)
+    hyde_result = hyde_embeddings.embed_query(commander_data.to_dict(as_series=False))
 
     hyde_search = vectorstore.similarity_search_by_vector_with_relevance_scores(
         hyde_result, k=10
@@ -54,9 +52,20 @@ def card_suggestions(commander):
 
     metadata_list = [document[0].metadata for document in hyde_search]
 
-    metadata_df = pl.DataFrame(metadata_list)
+    # log if commander card name in metadata
+    for item in metadata_list:
+        if commander in item["name"]:
+            log.info("Commander card in metadata", metadata=item)
 
-    return metadata_df
+    display_commander = commander_data.select(["name", "manaCost", "type", "text"])
+
+    display_metadata = (
+        pl.DataFrame(metadata_list)
+        .select(["name", "manaCost", "type", "text"])
+        .filter(~pl.col("name").str.contains(commander))
+    )
+
+    return display_commander, display_metadata
 
 
 demo = gr.Interface(
@@ -64,7 +73,11 @@ demo = gr.Interface(
     inputs=gr.Dropdown(
         commander_names, label="Comander", info="Select a commander for the deck"
     ),
-    outputs=gr.Dataframe(),
+    outputs=[  # commander data
+        gr.DataFrame(),
+        # metadata
+        gr.DataFrame(),
+    ],
 )
 
 llm = OpenAI()
